fe5780850dd1abd3f55e6865c7b82fc1da16e30f
laurenhatchett <laurenhatchett@gmail.com>
2023-02-08 23:44:30 -0500

    practice values and variables

---
+++ JS Fundamentals- Part 1/script.js
@@ -0,0 +1,24 @@
+let js = 'amazing'
+if (js === 'amazing') alert('JavaScript is FUN!')
+
+console.log(40 + 8 + 23 - 10)
+
+console.log('Jonas')
+console.log(23)
+
+let firstName = 'Matilda'
+console.log(firstName)
+
+let century = 'United States'
+let continent = 'North America'
+let population = 331900000
+
+console.log(country, continent, population)
+
+let isIsland = 'Unted States'
+isIsland = true
+
+let language
+
+console.log(isIsland)
+console.log(language)
