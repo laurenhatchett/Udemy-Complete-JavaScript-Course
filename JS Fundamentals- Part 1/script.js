1609f4f170128c88b88a433f5721ac5b26c879ea
laurenhatchett <laurenhatchett@gmail.com>
2023-02-08 23:46:34 -0500

    add examples to values and variables practice

--- JS Fundamentals- Part 1/script.js
+++ JS Fundamentals- Part 1/script.js
@@ -6,6 +6,11 @@ console.log(40 + 8 + 23 - 10)
 console.log('Jonas')
 console.log(23)
 
+console.log(40 + 8 + 23 - 10)
+
+console.log('Jonas')
+console.log(23)
+
 let firstName = 'Matilda'
 console.log(firstName)
 
