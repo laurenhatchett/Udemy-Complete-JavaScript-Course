000c0e3c752b32895521de05e2d82aa42962217b
laurenhatchett <laurenhatchett@gmail.com>
2023-02-08 23:57:45 -0500

    practice data types

--- JS Fundamentals- Part 1/script.js
+++ JS Fundamentals- Part 1/script.js
@@ -1,15 +1,14 @@
+
+/////////// Linking a JavaScript File
 let js = 'amazing'
 if (js === 'amazing') alert('JavaScript is FUN!')
-
 console.log(40 + 8 + 23 - 10)
+
+/////////////Values and Variables
 
 console.log('Jonas')
 console.log(23)
 
-console.log(40 + 8 + 23 - 10)
-
-console.log('Jonas')
-console.log(23)
 
 let firstName = 'Matilda'
 console.log(firstName)
@@ -27,3 +26,27 @@ let language
 
 console.log(isIsland)
 console.log(language)
+
+/////////// Data Types
+
+let javascriptIsFun= true
+console.log(javascriptIsFun)
+
+console.log(typeof true)
+console.log( typeof javascriptIsFun)
+
+console.log(typeof 23)
+console.log(typeof 'Jonas')
+
+javascriptIsFun = 'YES!'
+console.log(typeof javascriptIsFun)
+
+let year
+console.log(year)
+console.log(typeof year)
+
+year = 1991
+console.log(typeof year)
+
+console.log(typeof null)
+
